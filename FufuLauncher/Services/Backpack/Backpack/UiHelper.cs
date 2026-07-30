bbad7a81a3782f4f2cbc9a82658ba24f0800a1d8
CodeCubist <3688804669@qq.com>
2026-07-30 08:52:51 +0800

    更新

--- FufuLauncher/Services/Backpack/Backpack/UiHelper.cs
+++ FufuLauncher/Services/Backpack/Backpack/UiHelper.cs
@@ -1,3 +1,7 @@
+/*
+Copyright (c) FufuLauncher Dev Team. All rights reserved.
+Licensed under the MIT License.
+*/
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Media;
