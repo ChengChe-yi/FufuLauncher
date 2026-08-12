ccf3daa92765b8e683c1f926470b9fbd7a3adc3d
CodeCubist <3688804669@qq.com>
2026-08-12 12:06:10 +0800

    更新版本号

--- UpdateFufuLauncher/MainWindow.xaml.cs
+++ UpdateFufuLauncher/MainWindow.xaml.cs
@@ -47,7 +47,7 @@ namespace Updater
             public int AnimationId;
         }
         
-        private const string AppVersion = "1.5.2.0";
+        private const string AppVersion = "1.6.0.0";
 
         private static readonly HttpClient _httpClient = new(new HttpClientHandler
         {
