da1bd01cec99ec467678def89a29ed7c919dbec2
CodeCubist <3688804669@qq.com>
2026-09-05 18:51:10 +0800

    移除Backpack

--- FufuLauncher/Views/Main/PanelPage.xaml.cs
+++ FufuLauncher/Views/Main/PanelPage.xaml.cs
@@ -43,12 +43,6 @@ public sealed partial class PanelPage : Page
         window.Activate();
     }
 
-    private void OnOpenBackpackClick(object sender, RoutedEventArgs e)
-    {
-        var window = new BackpackWindow();
-        window.Activate();
-    }
-
     private void OnOpenPlayerRolesClick(object sender, RoutedEventArgs e)
     {
         var window = new PlayerInfoWindow();
